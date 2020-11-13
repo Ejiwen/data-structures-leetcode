24d65bfcfbf676ec776ec7eb834f11b28e7bb3a0
ejiwen <skibnewrim@gmail.com>
2020-11-12 23:31:33 -0800

    819. Most Common Word

--- leetcode/src/easy/leetcode/App.java
+++ leetcode/src/easy/leetcode/App.java
@@ -4,24 +4,36 @@ import java.util.*;
 
 public class App {
 
-    public static int[] targetValue(int[] arr, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<arr.length; i++) {
-            int k = target - arr[i];
-            if(map.containsKey(k)){
-                int[] a = {map.get(k),i};
-                return a;
-            } else {
-                map.put(arr[i],i);
+    public static void mostCommonWord(String paragraph, String[] banned) {
+
+        String[] str = paragraph.replaceAll("[^a-zA-Z ]", " ").toLowerCase().split(" ");
+        // String[] str = paragraph.replaceAll("[^a-zA-Z0-9 ]", " ").toLowerCase().split("\\s+");
+        String bannedStr = String.join(" ", banned);
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<str.length; i++) {
+            // System.out.println(str[i]);
+            if(!bannedStr.contains(str[i])) {
+                int j = map.containsKey(str[i]) ? map.get(str[i]) : 0;
+                map.put(str[i], j + 1);
             }
         }
-        int[] b = {-1,-1};
-        return b;
+
+        Map.Entry<String, Integer> maxEntry = null;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (maxEntry == null || entry.getValue()
+                    .compareTo(maxEntry.getValue()) > 0) {
+
+                maxEntry = entry;
+            }
+        }
+        System.out.println(maxEntry.getKey());
     }
 
     public static void main(String[] args) {
-        int[] arr = {3, 6, 2, 7};
+        String paragraph = "a, a, a, a, b,b,b,c, c";
+        String[] banned = {"a"};
 
-        System.out.println(targetValue(arr,8)[0] +"-----"+targetValue(arr,8)[1]);
+        //System.out.println(mostCommonWord(paragraph,banned));
+        mostCommonWord(paragraph,banned);
     }
 }
